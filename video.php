8431233623b7dc6419f52a131a3cc5abc0011b9e
BartoszKowalskiTCZ <bartek.kowalski20052004@gmail.com>
2023-04-19 13:34:06 +0200

    x

--- video.php
+++ video.php
@@ -2,27 +2,72 @@
 <head>
     <meta charset="UTF-8">
     <title>Video On Demand</title>
-   <link rel="stylesheet" href="styl3.css">
+    <link rel="stylesheet" href="styl3.css">
 </head>
 <body>
     <?php
-        $con = new mysqli("localhost", "root", "", "dane3");
+        $con = mysqli_connect("localhost", "root", "", "dane3");
+        if(mysqli_connect_errno()){
+            echo "nie udalo sie polaczyc, sprobuj jeszcze raz: " . mysqli_connect_error();
+            exit();
+        }
         if(isset($_POST["nr"]) && !empty($_POST["nr"])){
-        $sql = "DELETE FROM `produkty` WHERE `id` = '".$_POST["nr"]."';";
-        mysqli_query($con, $sql);
+            $sql = "DELETE FROM `produkty` WHERE `id` = '".$_POST["nr"]."'";
+            $con->query($sql);
         }
-        echo "<div class='lista'> 
-        <h3>Polecamy</h3>";
-        $sql="SELECT `id`, `nazwa`, `ilosc`, `opis`, `zdjecie` FROM `produkty` WHERE `id`=18 or `id`=22 or `id`=23 or `id`=25;";
-        $res = mysqli_query($con, $sql);
-        while($row = mysqli_fetch_assoc($res)){
-        echo "<div class='film'>
-        <h4>".$row["id"].". ".$row["nazwa"]."</h4>
-        <img src='".$row["zdjecie"]."' alt='film'>
-        <p>".$row["opis"]."</p>
-            </div>            
-            ";
-        }
-    
-    
-    
+    ?>
+    <div class="banerLewy">
+        <h1>Internetowa wypożyczalnia filmów</h1>
+    </div>
+    <div class="banerPrawy">
+        <table>
+            <tr>
+                <td>Kryminał</td>
+                <td>Horror</td>
+                <td>Przygodowy</td>
+            </tr>
+            <tr>
+                <td>20</td>
+                <td>30</td>
+                <td>20</td>
+            </tr>
+        </table>
+    </div>
+    <div class="lista">
+        <h3>Polecamy</h3>
+        <?php
+            $sql="SELECT `id`, `nazwa`, `ilosc`, `opis`, `zdjecie` FROM `produkty` WHERE `id`=18 or `id`=22 or `id`=23 or `id`=25";
+            $res = $con->query($sql);
+            while($row = $res->fetch_assoc()){
+                echo "<div class='film'>";
+                echo "<h4>" . $row["id"] . ". " . $row["nazwa"] . "</h4>";
+                echo "<img src='" . $row["zdjecie"] . "' alt='film'>";
+                echo "<p>" . $row["opis"] . "</p>";
+                echo "</div>";
+            }
+        ?>
+    </div>
+    <div class="lista">
+        <h3>Filmy fantastyczne</h3>
+        <?php
+            $sql="SELECT `id`, `nazwa`, `ilosc`, `opis`, `zdjecie` FROM `produkty` WHERE `Rodzaje_id` = 12";
+            $res = $con->query($sql);
+            while($row = $res->fetch_assoc()){
+                echo "<div class='film'>";
+                echo "<h4>" . $row["id"] . ". " . $row["nazwa"] . "</h4>";
+                echo "<img src='" . $row["zdjecie"] . "' alt='film'>";
+                echo "<p>" . $row["opis"] . "</p>";
+                echo "</div>";
+            }
+        ?>
+    </div>
+    <div class="stopka">
+        <form method="POST">
+            <label>Usuń film nr.:</label><input type="number" name="nr">
+            <input type="submit" value="Usuń film">
+        </form>
+        <span>Stronę wykonał: bartosz kowalski</span>
+    </div>
+    <?php mysqli_close($con); ?>
+    </body>
+</html>
